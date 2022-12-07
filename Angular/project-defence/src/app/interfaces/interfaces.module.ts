ea8cded099f7a7fb5bab62de2349182e44cc6b05
instamenta <98179343+instamenta@users.noreply.github.com>
2022-12-08 00:22:43 +0200

    added header and footer working on homepage and basic architecture implementation

---
+++ Angular/project-defence/src/app/interfaces/interfaces.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeViewComponent } from './home-view/home-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeViewComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeViewComponent
+  ]
+})
+export class InterfacesModule { }
