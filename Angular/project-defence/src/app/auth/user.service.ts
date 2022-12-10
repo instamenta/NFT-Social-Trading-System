544ffb98a2691742f323f218d4a5a1504a5cecac
instamenta <98179343+instamenta@users.noreply.github.com>
2022-12-10 16:56:34 +0200

    user services added

--- Angular/project-defence/src/app/auth/user.service.ts
+++ Angular/project-defence/src/app/auth/user.service.ts
@@ -8,5 +8,21 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  registerUser(username: any, email: any ,birthday: any, password: any) {
+    return this.http.post('http://localhost:3031/users/register', { username,email,birthday,password })
+  } 
+  loginUser(username: any, password: any) {
+    return this.http.post('http://localhost:3031/users/login', {username, password})
+  }
+  getUser(id: any) {
+    return this.http.get('http://localhost:3031/profile/' + id)
+  }
+  editUser(id: any ,username:any, email: any) {
+    return this.http.post(`http://localhost:3031/profile/${id}/edit`, { username, email })
+  }
+  editUserDescription(id: any, description: any) {
+    return this.http.post(`http://localhost:3031/profile/${id}/edit-bio`, { description })
+  }
+  // editUserProfilePicture()
   
 }
