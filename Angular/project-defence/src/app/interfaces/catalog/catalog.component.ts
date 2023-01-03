1a629213e32ef2c90235e61b09af00db1fd0d07e
instamenta <98179343+instamenta@users.noreply.github.com>
2023-01-03 22:40:58 +0200

    catalog search updated

--- Angular/project-defence/src/app/interfaces/catalog/catalog.component.ts
+++ Angular/project-defence/src/app/interfaces/catalog/catalog.component.ts
@@ -1,45 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { NftService } from 'src/app/auth/nft.service';
+import { UserService } from 'src/app/auth/user.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss']
 })
-export class CatalogComponent implements OnInit{
-  nftsData: any | null = null  
+export class CatalogComponent implements OnInit {
+  nftsData: any | null = null
 
-  searchText: any;
+  searchText: String = '';
   searchCriteria: any = false;
   searchData: any = [];
   notFound: any = false
-  notSearching:any = true;
+  notSearching: any = true;
 
   checkboxValue: any = false;
   checkboxText: any = true;
-  constructor(private nftService: NftService) {}
+
+  currentUserData: any = false;
+  constructor(
+    private nftService: NftService,
+    private userService: UserService,
+
+  ) { }
 
   ngOnInit(): void {
     this.nftService.loadNfts()
-    .subscribe((data) => { this.nftsData = data})
+      .subscribe((data) => { this.nftsData = data })
+    this.userService.getUserData()
+      .subscribe(userData => {
+        if (!userData.hasOwnProperty('username')) return
+        this.currentUserData = userData
+      })
   }
   search() {
     this.searchData = this.nftsData.filter((data: any) => {
-      return data.name.toLowerCase().includes(this.searchText.toLowerCase())
-  });
-    if(this.searchData.length > 0) {
+      if (this.checkboxValue == true) {
+        return data.name.toLowerCase().includes(this.searchText.toLowerCase()) && !data.owners.includes(this.currentUserData.username)
+      } else {
+        return data.name.toLowerCase().includes(this.searchText.toLowerCase())
+      }
+    });
+    if (this.searchData.length > 0) {
       this.searchCriteria = true
       this.notFound = false
-      
-      if(this.searchText.length <= 0) {
+
+      if (this.searchText.length <= 0) {
         this.notSearching = true;
       } else {
         this.notSearching = false;
       }
     } else {
       this.searchCriteria = false
-      
-      if(this.searchText.length > 0) {
+
+      if (this.searchText.length > 0) {
         this.notFound = true
         this.notSearching = false;
       } else {
@@ -48,12 +64,26 @@ export class CatalogComponent implements OnInit{
     }
   }
   switchHandler($event: any) {
-    console.log(this.checkboxValue)
-    console.log($event.target)
-    if(this.checkboxValue == true) {
+    if (this.checkboxValue == true) {
       this.checkboxText = true;
+      this.searchData = this.nftsData.filter((data: any) => {
+        if (this.searchText.length > 0) {
+          console.log(1)
+          return data.name.toLowerCase().includes(this.searchText.toLowerCase())
+        }
+        console.log(2)
+        return data.hasOwnProperty('name')
+      })
     } else {
       this.checkboxText = false;
+      const dataArray = this.nftsData.filter((data: any) => {
+        if (this.searchText.length > 0) {
+          return data.name.toLowerCase().includes(this.searchText.toLowerCase()) && !data.owners.includes(this.currentUserData.username)
+        }
+        return !data.owners.includes(this.currentUserData.username)
+      })
+      this.searchData = dataArray
+      console.log(dataArray)
     }
   }
 }
